25bbeed5f870314793b0ad5548fc47e17203a90b
KateandEva <pavlovskakate2002@gmail.com>
2019-11-01 11:38:24 +0200

    new package

---
+++ StudentFunction/StudentParcer.py
@@ -0,0 +1,10 @@
+def parce_card(sequence):
+    """
+
+    :param sequence: тип: рядок, звідки: введено користувачем, формат:(серія)№(номер), приклад: КВ№12345678
+    :return: тип: рядок, формат: (серія), (номер), приклад: КВ, 12345678
+    """
+    print(sequence)
+    return None
+if __name__ == '__main__':
+    parce_card('smth')
